88f83a8e3496d8fb9a98e3637dc76c5311ea506d
DanaMichelleZ <dana.zambelli@davinci.edu.ar>
2024-11-17 19:07:03 -0300

    empezamos fr

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Proyecto Vocaloid uwu</h1>
+      </header>
+      <main>
+        <p>Bienvenido a la API Vocaloid :3</p>
+      </main>
+      <footer>
+        <p>Aplicaciones Híbridas, Parcial 2 - Dana Michelle Zambelli</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;

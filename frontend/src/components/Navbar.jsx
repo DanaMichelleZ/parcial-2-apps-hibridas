1a0e4be178690375c243d9eeefb8238d3cff68dd
DanaMichelleZ <dana.zambelli@davinci.edu.ar>
2024-11-19 17:22:03 -0300

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    console.log("Cerrando sesión...");
+    navigate("/login");
+  };
+
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Proyecto Vocaloid</a>
+        <Link className="navbar-brand" to="/">
+          Home
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -20,24 +29,14 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/vocaloids">
-                Vocaloids
+              <Link className="nav-link" to="/vocaloids">
+                Lista de Vocaloids
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/features">
-                Features
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/pricing">
-                Pricing
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link disabled" aria-disabled="true" to="#">
-                Disabled
-              </Link>
+              <button className="btn btn-danger nav-link" onClick={handleLogout}>
+                Cerrar Sesión
+              </button>
             </li>
           </ul>
         </div>
